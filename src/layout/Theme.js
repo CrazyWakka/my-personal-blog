0ff52b58c556061199cb0821df96cdfe4dce3cb3
Andy Guice <andy_guice@hotmail.com>
2021-01-01 13:47:39 -0600

    remove unused deps

--- src/layout/Theme.js
+++ src/layout/Theme.js
@@ -1,19 +1,22 @@
 export default {
   useCustomProperties: true,
-  useColorSchemeMediaQuery: true,
+  breakpoints: ["40em", "52em", "64em"],
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fonts: {
-    body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
+    body:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
     heading: "inherit",
+    monospace: "Menlo, monospace",
   },
-  fontSizes: [16, 18, 20, 24, 30, 36, 40, 48, 64, 72, 96],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
   fontWeights: {
-    lite: 200,
     body: 400,
     heading: 700,
+    bold: 700,
   },
   lineHeights: {
     body: 1.5,
-    heading: 1.25,
+    heading: 1.125,
   },
   images: {
     avatar: {
@@ -25,18 +28,6 @@ export default {
     primary: "#220354",
     secondary: "#27a727",
     background: "#ddd",
-    text: "#1b1e21",
-    blue: "#4169e1",
-    cyan: "#41b9e1",
-    gray: "#aeb3c0",
-    green: "#27a727",
-    purple: "#6941e1",
-    orange: "#fba100",
-    pink: "#e141b9",
-    red: "#ee5555",
-    white: "#fff",
-    yellow: "#FFDD22",
-    lite: "#eee",
     modes: {
       dark: {
         text: "#fff",
@@ -46,20 +37,66 @@ export default {
       },
     },
   },
-  space: [0, 4, 8, 16, 32, 64, 128],
-  breakpoints: ["32em", "48em", "64em", "80em"],
-  radii: [0, 3, 6],
-  shadows: {
-    card: "0 0 4px rgba(0, 0, 0, .125)",
-    sm: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
-    md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
-    lg:
-      "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-    xl:
-      "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-    "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
-    outline: "0 0 0 3px rgba(66, 153, 225, 0.6)",
-    inner: "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
-    none: "none",
+  text: {
+    heading: {
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+    },
+  },
+  styles: {
+    root: {
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body",
+    },
+    h1: {
+      variant: "text.heading",
+      fontSize: 5,
+    },
+    h2: {
+      variant: "text.heading",
+      fontSize: 4,
+    },
+    h3: {
+      variant: "text.heading",
+      fontSize: 3,
+    },
+    h4: {
+      variant: "text.heading",
+      fontSize: 2,
+    },
+    h5: {
+      variant: "text.heading",
+      fontSize: 1,
+    },
+    h6: {
+      variant: "text.heading",
+      fontSize: 0,
+    },
+    pre: {
+      fontFamily: "monospace",
+      overflowX: "auto",
+      code: {
+        color: "inherit",
+      },
+    },
+    code: {
+      fontFamily: "monospace",
+      fontSize: "inherit",
+    },
+    table: {
+      width: "100%",
+      borderCollapse: "separate",
+      borderSpacing: 0,
+    },
+    th: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
+    },
+    td: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
+    },
   },
 }
--- src/ui/ThemeToggle.js
+++ src/ui/ThemeToggle.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
-import { Box, useColorMode } from "theme-ui"
+// import { Box, useColorMode } from "theme-ui"
 import Button from "./Button"
 import { IconButton } from "theme-ui"
 
 const ThemeToggle = (props) => {
-  const [colorMode, setColorMode] = useColorMode()
+  // const [colorMode, setColorMode] = useColorMode()
   const [opacity, setOpacity] = useState(0)
 
   useEffect(() => {
@@ -13,25 +13,26 @@ const ThemeToggle = (props) => {
   }, [])
 
   return (
-    <Box
-      sx={{
-        p: 3,
-        position: "absolute",
-        top: 0,
-        right: 0,
-        opacity,
-        transition: "opacity .25s ease-in-out",
-      }}
-    >
-      <Button
-        sx={{ bg: "gray", py: 1, px: 2, fontSize: 0 }}
-        onClick={(e) => {
-          setColorMode(colorMode === "default" ? "dark" : "default")
-        }}
-      >
-        {colorMode === "default" ? "dark" : "light"} theme
-      </Button>
-    </Box>
+    <div>Theme thing</div>
+    // <Box
+    //   sx={{
+    //     p: 3,
+    //     position: "absolute",
+    //     top: 0,
+    //     right: 0,
+    //     opacity,
+    //     transition: "opacity .25s ease-in-out",
+    //   }}
+    // >
+    //   <Button
+    //     sx={{ bg: "gray", py: 1, px: 2, fontSize: 0 }}
+    //     onClick={(e) => {
+    //       setColorMode(colorMode === "default" ? "dark" : "default")
+    //     }}
+    //   >
+    //     {colorMode === "default" ? "dark" : "light"} theme
+    //   </Button>
+    // </Box>
   )
 }
 
